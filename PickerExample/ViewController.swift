cf049be2f5d0f879f1b7577fc18953279ea41281
Mae Yee <mae@Alans-iMac.local>
2015-02-21 12:54:22 -0800

    Initial Commit

---
+++ PickerExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PickerExample
+//
+//  Created by Mae Yee on 2015-02-21.
+//  Copyright (c) 2015 Mae Yee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
